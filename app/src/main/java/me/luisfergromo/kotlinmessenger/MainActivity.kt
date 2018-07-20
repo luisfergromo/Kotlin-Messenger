7c871782b05c129ada747acf7cf1dedfc752cec1
Luis Fernando Gutiérrez Romo <mobilelab@iteso.mx>
2018-07-19 20:50:39 -0500

    Video 2 4:15

--- app/src/main/java/me/luisfergromo/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/me/luisfergromo/kotlinmessenger/MainActivity.kt
@@ -3,8 +3,8 @@ package me.luisfergromo.kotlinmessenger
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.util.Log.wtf
-import android.view.View
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_main.*
@@ -29,20 +29,21 @@ class MainActivity : AppCompatActivity() {
             val email = email_editText.text.toString()
             val password = password_editText.text.toString()
 
-            wtf("MainActivity","Email is: $email")
-            wtf("MainActivity", "Password: $password")
+            Log.d("RegisterBtn", "Email is: " + email)
+            wtf("RegisterBtn", "Password: $password")
+
+            //Firebase Auth to create a user with email and password
         }
         alreadyUser_TextView.setOnClickListener {
             //wtf("MainActivity","Try to show login activity")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-        registerBtn.setOnClickListener {
-        }
     }
 
+    /*
     fun forceCrash(view: View) {
         throw RuntimeException("This is a crash")
     }
-
+ */
 }
